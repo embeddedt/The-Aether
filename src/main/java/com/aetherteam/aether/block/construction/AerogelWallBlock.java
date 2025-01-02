af072a8a96838d4d9743ef6b1df463833b9a6115
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-02 16:58:21 -0500

    fix: avoid regressing #1935

--- src/main/java/com/aetherteam/aether/block/construction/AerogelWallBlock.java
+++ src/main/java/com/aetherteam/aether/block/construction/AerogelWallBlock.java
@@ -5,11 +5,25 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.WallSide;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class AerogelWallBlock extends WallBlock {
+    private static final Map<Direction, EnumProperty<WallSide>> WALL_SIDES_BY_DIRECTION = new EnumMap<>(Direction.class);
+
+    static {
+        WALL_SIDES_BY_DIRECTION.put(Direction.NORTH, NORTH_WALL);
+        WALL_SIDES_BY_DIRECTION.put(Direction.SOUTH, SOUTH_WALL);
+        WALL_SIDES_BY_DIRECTION.put(Direction.EAST, EAST_WALL);
+        WALL_SIDES_BY_DIRECTION.put(Direction.WEST, WEST_WALL);
+    }
+
     public AerogelWallBlock(Properties properties) {
         super(properties);
     }
@@ -39,6 +53,18 @@ public class AerogelWallBlock extends WallBlock {
      * This prevents adjacent walls from rendering redundant faces.
      */
     protected boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction side) {
-        return adjacentBlockState.is(this) || super.skipRendering(state, adjacentBlockState, side);
+        if (adjacentBlockState.is(this)) {
+            if (side.getAxis().isHorizontal()) {
+                // Verify that both walls have the same height. This avoids culling a face that actually sticks out
+                // above another face.
+                WallSide ourHeight = state.getValue(WALL_SIDES_BY_DIRECTION.get(side));
+                WallSide theirHeight = adjacentBlockState.getValue(WALL_SIDES_BY_DIRECTION.get(side.getOpposite()));
+                return ourHeight.ordinal() <= theirHeight.ordinal();
+            } else {
+                // If we render a tall post face, the wall above must also have a tall post to skip rendering
+                return !state.getValue(UP) || adjacentBlockState.getValue(UP);
+            }
+        }
+        return super.skipRendering(state, adjacentBlockState, side);
     }
 }
